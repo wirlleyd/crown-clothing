2d757e0a4268fd11c95140ef51723c6ac78cb0ad
Wirlley Delfino <wirlley_d@hotmail.com>
2020-03-03 23:37:58 -0300

    Shop page creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import { Route } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage'
+import ShopPage from './pages/shop/shop'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./hompage.component.scss";
 import Directory from "../../components/directory/directory";
 
-export const HomePage = () => (
+export const HomePage = ({ history }) => (
   <div className="homepage">
     <Directory />
   </div>
