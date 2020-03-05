e70ef0fd55e3b0ebf666358ef66ceccbf0d4a004
Wirlley Delfino <wirlley_d@hotmail.com>
2020-03-04 23:38:16 -0300

    Creating login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
-import { HomePage } from './pages/homepage/homepage'
-import ShopPage from './pages/shop/shop'
+import { Route, Switch } from 'react-router-dom';
+import { HomePage } from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header.js';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
+      </Switch>
     </div>
   );
 }
