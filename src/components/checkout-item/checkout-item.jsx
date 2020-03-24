b1201c6a2a7a2c4b88b845b8ef14762389ccd0ae
Wirlley Delfino <wirlley_d@hotmail.com>
2020-03-23 22:35:00 -0300

    Create remove function from checkout page

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./checkout-item.scss";
-
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
-  console.log("image", imageUrl);
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import { connect } from "react-redux";
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -11,9 +12,15 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToState = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToState)(CheckoutItem);
