a8233a1e0ad613905dca5209492b5b3c2a4ebf88
Wirlley Delfino <wirlley_d@hotmail.com>
2020-03-27 09:05:26 -0300

    Renaming Title

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import {
-  selectShopCollections,
-  selectCollectionsForPreview
-} from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import "./collections-overview.scss";
 import CollectionPreview from "../collection-preview/collection-preview";
 const CollectionsOverview = ({ collections }) => (
